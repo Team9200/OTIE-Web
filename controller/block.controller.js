9b24bcad9d6d03678dd0d386688a8ef777994674
Smaker <admin@smaker.kr>
2018-11-16 02:39:59 +0900

    Update block.controller.js

---
+++ controller/block.controller.js
@@ -0,0 +1,15 @@
+import Block from '../database/models/block';
+
+function get(req, res) {
+    
+}
+
+function search(req, res) {
+    var type = req.body.type;
+
+}
+
+export default {
+    get,
+    search
+};
