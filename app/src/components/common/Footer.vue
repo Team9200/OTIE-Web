5ae6c7b608b87c0d51f9ce6bcc44e850991df212
Smaker <admin@smaker.kr>
2018-11-15 20:59:50 +0900

    Update front

---
+++ app/src/components/common/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-footer color="indigo" app>
+      <div class="container">
+        <span class="white--text">&copy; Open Threat Intelligence Ecosystem</span>
+      </div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: 'footer'
+}
+</script>
+
--- app/src/main.js
+++ app/src/main.js
@@ -1,28 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
-import HomePage from './components/HomePage/HomePage'
-import Navigation from './components/common/Navigation'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+import Vuetify from 'vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import 'vuetify/dist/vuetify.min.css';
+import 'babel-polyfill'
 
-// import Antd from 'ant-design-vue'
-// import 'ant-design-vue/lib/style'
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+Vue.use(Vuetify, {
+  iconfont: 'mdi'
+});
 
-// Vue.use(Antd)
-
-Vue.prototype.$http = axios
-Vue.config.productionTip = false
-
-Vue.components(HomePage);
-Vue.components(Navigation);
-
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app');
