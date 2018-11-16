40f1e0d7af96c8a36254f11567f57064681ea5f0
NGA <40926806+ksh95216@users.noreply.github.com>
2018-11-16 22:41:56 +0900

    Update block.controller.js

--- controller/block.controller.js
+++ controller/block.controller.js
@@ -1,15 +1,45 @@
 import Block from '../database/models/block';
 
 function get(req, res) {
-    
+
+	Block.find({}, function(err, data){
+
+    	if(err){
+    	
+    		res.json({
+            	success: false,
+            	message: 'Query error'
+        	});
+    	}
+    	
+    	console.log(data);	// 결과
+
+    });
+
 }
 
 function search(req, res) {
-    var type = req.body.type;
+
+    var query = req.body.query; // get query
+
+   
+   	Block.find({"index":query}, function(err, data){
+
+    	if(err){
+    	
+    		res.json({
+            	success: false,
+            	message: 'Query error'
+        	});
+    	}
+    	
+    	console.log(data);	// 결과
+
+    });
 
 }
 
+
 export default {
-    get,
-    search
+    get, search
 };
