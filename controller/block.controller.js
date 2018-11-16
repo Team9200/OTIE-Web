6089ee74c49e7329cf2496e3164fcbc09d9234d8
NGA <40926806+ksh95216@users.noreply.github.com>
2018-11-17 05:04:43 +0900

    Update block.controller.js

--- controller/block.controller.js
+++ controller/block.controller.js
@@ -1,42 +1,56 @@
 import Block from '../database/models/block';
 
 function get(req, res) {
-    Block.find({}, function (err, data) {
-        if (err) {
-            return res.json({
+
+    var page = req.query.page;
+	Block.find({}.skip(10*(page-1)).limit(10), function(err, data){
+
+        if(err){
+        
+            res.json({
                 success: false,
                 message: 'Query error'
             });
-        } else {
-            return res.json({
+        }else{
+
+            res.json({
                 success: true,
                 message: data
             });
+
         }
+
     });
+
 }
 
 function search(req, res) {
-    var query = req.query.query; // get query
 
-    Block.find({
-        "index": query
-    }, function (err, data) {
-        if (err) {
-            return res.json({
+    var query = req.body.query; // get query
+    var page = req.query.page;
+
+   	Block.find({"index":query}.skip(10*(page-1)).limit(10), function(err, data){
+
+        if(err){
+        
+            res.json({
                 success: false,
                 message: 'Query error'
             });
-        } else {
-            return res.json({
+        }else{
+
+            res.json({
                 success: true,
                 message: data
             });
+
         }
+
     });
+
 }
 
+
 export default {
-    get,
-    search
+    get, search
 };
