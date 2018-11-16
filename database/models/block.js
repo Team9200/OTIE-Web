d3f5eb799db5551474dd49cc811f4e2e915283b7
NGA <40926806+ksh95216@users.noreply.github.com>
2018-11-16 22:04:35 +0900

    Update block.js

--- database/models/block.js
+++ database/models/block.js
@@ -1,33 +1,52 @@
 import mongoose from 'mongoose';
-import Malware from './malware';
 
 class Block extends mongoose.Schema {
     constructor() {
         const block = super({
+
             index: {
                 type: Number
             },
             timestamp: {
                 type: Number
             },
-            transactionList: [{
-                amout: {
-                    type: Number
-                },
-                sender: {
-                    type: String
-                },
-                recipient: {
-                    type: String
-                }
-            }],
-            malwaresList: [{
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Malware'
-            }]
+            transactionList: {
+                type: JSON
+
+            },
+            malwaresList: {
+                type: JSON
+            },
+            nonce: {
+                type: Number
+
+            },
+            hash: {
+                type: String
+            },
+            previousBlockHash: {
+                type : String
+            }
+
         });
+        block.statics.create = this.create;
 
         return block;
+    }
+    create(index, timestamp, transactionList, malwaresList, nonce, hash, previousBlockHash) {
+        const block = new this({
+            
+            index, 
+            timestamp,
+            transactionList, 
+            malwaresList,
+            nonce, 
+            hash, 
+            previousBlockHash
+        
+        });
+
+        return block.save();
     }
 }
 
