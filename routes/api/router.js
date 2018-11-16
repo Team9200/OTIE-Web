640e4700b0492fc2467e4b389611cc603ed8d5be
Smaker <jiheon.unidev@gmail.com>
2018-11-17 02:47:22 +0900

    Update route

--- routes/api/router.js
+++ routes/api/router.js
@@ -3,11 +3,19 @@ const router = express.Router();
 
 import authMiddleware from '../../middleware/auth';
 import UserController from '../../controller/user.controller';
+import malwareController from '../../controller/malware.controller';
+import blockController from '../../controller/block.controller';
 
 // UserController function: test, signup, signin, profile
 router.route('/user/test').get(UserController.test);
 router.route('/user/signin').post(UserController.signin);
 router.route('/user/signup').post(UserController.signup);
 router.route('/user/profile').get(authMiddleware, UserController.profile);
+
+router.route('/malware/get').get(malwareController.get);
+router.route('/malware/search').get(malwareController.search);
+
+router.route('/block/get').get(blockController.get);
+router.route('/block/search').get(blockController.get);
 
 export default router;
