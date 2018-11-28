b5906d99240f99802e7f4c3dff0a21c512828f9d
NGA <40926806+ksh95216@users.noreply.github.com>
2018-11-28 18:55:58 +0900

    pagination update

--- frontend/app.js
+++ frontend/app.js
@@ -3,9 +3,11 @@ import exphbs from 'express-handlebars';
 import request from 'request';
 import path from 'path';
 import 'dotenv/config';
+import pagination from './modules/pagination';
 
 const app = express();
 const API_URL = "http://openti.info:5000";
+
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -45,6 +47,7 @@ var hbs = exphbs.create({
     }
 });
 
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -52,123 +55,162 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/search/detail', function (req, res) {
-    var type = req.query.type;
-    var query = req.query.query;
-
-    request.get({url: API_URL + '/api/malware/search', qs: {type: type, query: query}}, function (err, response, body) {
-        body = JSON.parse(body);
-        res.render('search/detail', {
-            result: body.message,
-            query: query
-        }); 
-    });
-});
-
 app.get('/search/detail/:id', function (req, res) {
+
     var type = req.query.type;
     var query = req.query.query;
 
     request.get({url: API_URL + '/api/malware/search', qs: {type: type, query: query, page: req.params.id}}, function (err, response, body) {
+
         body = JSON.parse(body);
+        var page = pagination.create(body.max, 10, req.params.id, type, query, 1);
+
         res.render('search/detail', {
+
             result: body.message,
             type: type,
             query: query,
-        }); 
-    });
-});
+            page : page
 
-app.get('/board', function (req, res) {
-    request.get({
-        url: API_URL + '/api/malware/get'
-    }, function (err, response, body) {
-        body = JSON.parse(body);
-        res.render('board/board', {
-            result: body.message
-        });
+        }); 
+
     });
+
 });
 
 app.get('/board/:id', function (req, res) {
+
     request.get({
+
         url: API_URL + '/api/malware/get',
+
         qs: {
+
             page: req.params.id
+
         }
+
     }, function (err, response, body) {
+
         body = JSON.parse(body);
+        var page = pagination.create(body.max, 10, req.params.id,"","",0);
+
         res.render('board/board', {
-            result: body.message
+
+            result: body.message,
+            page : page
+
         });
+
     });
+
 });
 
 app.get('/login', function (req, res) {
+
     res.render('user/login');
+
 });
 
 app.get('/register', function (req, res) {
+
     res.render('user/register');
+
 });
 
 app.get('/block/', function (req, res) {
+
     request.get({url: API_URL + '/api/block/get'}, function (err, response, body) {
+
         body = JSON.parse(body);
+
         res.render('block/block', {
+
             result: body.message,
             type: req.query.type,
             query: req.query.qwuery
+
         });
+
     });
+
 });
 
 app.get('/block/detail/:id', function (req, res) {
+
     request.get({url: API_URL + '/api/block/search', qs: {query: req.params.id}}, function (err, response, body) {
+
         body = JSON.parse(body);
+
         res.render('block/detail', {
+
             result: body.message,
             type: req.query.type,
             query: req.query.qwuery
+
         });
+
     });
+
 });
 
 app.get('/status', function (req, res) {
+
     res.render('status', {});
+
 });
 
 app.get('/profile/:username', function (req, res) {
+
     request.get({
+
         url: API_URL + '/api/malware/search',
         qs: {
             type: 'ana',
             query: req.params.username
         }
+
     }, function (err, response, body) {
+
         body = JSON.parse(body);
+
         res.render('user/profile', {
+
             result: body.message,
             username: req.params.username
+
         });
+
     });
+
 });
 
 app.get('/board/detail/:azid', function (req, res) {
+
     request.get({
+
         url: API_URL + '/api/malware/search',
         qs: {
             type: 'azid',
             query: req.params.azid
         }
+
     }, function (err, response, body) {
+
         body = JSON.parse(body);
+
         res.render('board/detail', {
+
             result: body.message
+
         });
+
     });
+
 });
 
 app.listen(process.env.PORT, function () {
+
     console.log('frontend server on!');
+
 });
