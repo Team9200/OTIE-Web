624e5dc219c25b92527157738435383554cb0155
NGA <40926806+ksh95216@users.noreply.github.com>
2018-12-07 22:42:44 +0900

    update

--- frontend/src/components/ProfileView.vue
+++ frontend/src/components/ProfileView.vue
@@ -75,10 +75,15 @@
 
         <!-- Popular post start -->
 
-        <v-flex v-if="activity.length >= 1" lg7 sm12 xs12>
+        <v-flex v-if="listData1.length >= 1" lg7 sm12 xs12>
           <v-card>
             <v-toolbar card dense color="transparent">
-              <v-toolbar-title><h4>Popular post</h4></v-toolbar-title>
+              <v-toolbar-title>
+
+              <h4 v-if="nodeType === 'analyzer' ">Popular post</h4>
+              <h4 v-else-if="nodeType === 'collector'">Unique sample</h4>
+
+              </v-toolbar-title>
               <v-spacer></v-spacer>
               <v-btn icon>
                 <v-icon>more_vert</v-icon>
@@ -88,8 +93,8 @@
             <v-card-text class="pa-0">
               <template>
                 <v-data-table
-                  :headers="Ppheaders"
-                  :items="Ppdata"
+                  :headers="listHeader1"
+                  :items="listData1"
                   class="elevation-0"
                 >
                   <template slot="items" slot-scope="props">
@@ -97,7 +102,7 @@
                     <td>{{ props.item.id }}</td>
                     <td>{{ props.item.subject }}</td>
                     <td class="text-xs-left">{{ props.item.date}}</td>
-                    <td class="text-xs-left"><v-progress-linear :value="props.item.progress" height="5" :color="props.item.color"></v-progress-linear> </td>
+                    <td class="text-xs-left"><v-progress-linear v-if="props.item.progress >= 0" :value="props.item.progress" height="5" :color="props.item.color"></v-progress-linear> </td>
                 
                   </template>
                 </v-data-table>
@@ -113,7 +118,10 @@
 
            <v-card>
             <v-toolbar card dense color="transparent">
-              <v-toolbar-title><h4>Post</h4></v-toolbar-title>
+              <v-toolbar-title>
+              <h4 v-if="nodeType === 'analyzer' ">Post</h4>
+              <h4 v-else-if="nodeType === 'collector'">Sample</h4>
+              </v-toolbar-title>
               <v-spacer></v-spacer>
               <v-btn icon>
                 <v-icon>more_vert</v-icon>
@@ -123,8 +131,8 @@
             <v-card-text class="pa-0">
               <template>
                 <v-data-table
-                  :headers="Pheaders"
-                  :items="Pdata"
+                  :headers="listHeader2"
+                  :items="listData2"
                   class="elevation-0 table-striped"
                 >
                   <template slot="items" slot-scope="props">
@@ -174,7 +182,7 @@ export default {
     monthActivity: [],
     tag: [], 
     activity: [],
-    Ppheaders: [
+    listHeader1: [
 
       {
         text: '#',
@@ -192,45 +200,8 @@ export default {
       { text: 'Grade', sortable: false, value: 'progress' },
  
     ],
-    Ppdata: [
-      {
-        id: '1',
-        subject: 'Template PSD',
-        date: '2018-12-07 13:06:54',
-        progress: 90,
-        color: 'pink',
-      },
-      {
-        id: '2',    
-        subject: 'Logo Design',
-        date: '2018-12-07 13:06:54',
-        progress: 70,
-        color: 'success'
-      },
-      {
-        id: '3',    
-        subject: 'REST API',
-        date: '2018-12-07 13:06:54',
-        progress: 50,
-        color: 'info'
-      },
-      {
-        id: '4',    
-        subject: 'API Unit Test',
-        date: '2018-12-07 13:06:54',
-        progress: 30,
-        color: 'teal'
-      },
-      {
-        id: '5',    
-        subject: 'Project Deploy',
-        date: '2018-12-07 13:06:54',
-        progress: 15,
-        color: 'grey'
-      },
-
-    ],
-    Pheaders: [
+    listData1: [{},{},{},{},{}],
+    listHeader2: [
 
       {
         text: '#',
@@ -247,34 +218,7 @@ export default {
       { text: 'Date', sortable: false, value: 'date' },
  
     ],    
-    Pdata: [
-      {
-        id: '1',
-        subject: 'Template PSD',
-        date: '2018-12-07 13:06:54',
-      },
-      {
-        id: '2',    
-        subject: 'Logo Design',
-        date: '2018-12-07 13:06:54',
-      },
-      {
-        id: '3',    
-        subject: 'REST API',
-        date: '2018-12-07 13:06:54',
-      },
-      {
-        id: '4',    
-        subject: 'API Unit Test',
-        date: '2018-12-07 13:06:54',
-      },
-      {
-        id: '5',    
-        subject: 'Project Deploy',
-        date: '2018-12-07 13:06:54',
-      },
-
-    ],
+    listData2: [{},{},{},{},{}],
   }),
   computed: {
 
@@ -286,6 +230,7 @@ export default {
       const name = this.$route.query.name;
       const type = this.$route.query.type;
       this.nodeType = type;
+      console.log(this.nodeType);
 
       apiService.searchNode(type, name)
         .then(response => {
@@ -294,6 +239,7 @@ export default {
           this.init_tagChart(malwareList);
           this.init_mActive(malwareList);
           this.init_Active(malwareList);
+          this.init_List(malwareList);
         
         });
       
@@ -431,6 +377,37 @@ export default {
       this.activity = result;
       console.log('created activity chart');
 
+    },
+    init_List (malwareList) {
+
+      let result1 = [];
+      let result2 = [];
+      const gradeColor = ['grey','pink','teal','success','info'];
+
+      malwareList.forEach(function (data, i) {
+
+        let tmp1 = {};
+        let tmp2 = {};
+
+        tmp1.id = i;
+        tmp1.subject = (data.azid).slice(0,30)+'...';
+        tmp1.date = new Date(data.date).toLocaleString();
+        tmp1.progress = 25*i;
+        tmp1.color = gradeColor[i];
+      
+        tmp2.id = i;
+        tmp2.subject = (data.azid).slice(0,20)+'...';
+        tmp2.date = new Date(data.date).toLocaleString();
+
+        result1.push(tmp1)
+        result2.push(tmp2);
+
+      });
+
+      this.listData1 = result1;
+      this.listData2 = result2;
+      console.log("created List Data");
+
     }
   },
   created: function () {
@@ -441,160 +418,6 @@ export default {
 
 };
 </script>
-<style>
-  .timeline-point-sm .timeline-point {
-      width: 64px;
-  }
-
-  .timeline-point {
-    position: relative;
-    -ms-flex-negative: 0;
-    flex-shrink: 0;
-    -webkit-box-flex: 0;
-    -ms-flex-positive: 0;
-    flex-grow: 0;
-    -webkit-box-ordinal-group: 3;
-    -ms-flex-order: 2;
-    order: 2;
-    width: 96px;
-    padding-bottom: 30px;
-    text-align: center;
-}
-  .timeline-point::before {
-    content: '';
-    position: absolute;
-    top: 9px;
-    left: 50%;
-    bottom: -9px;
-    width: 1px;
-    margin-left: -1px;
-    z-index: -1;
-    background-color: #ebebeb;
-}
-  ::before, ::after {
-    text-decoration: inherit;
-    vertical-align: inherit;
-}
-*, ::before, ::after {
-    -webkit-box-sizing: inherit;
-    box-sizing: inherit;
-}
-  .timeline .timeline-block:first-child .timeline-point::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 50%;
-    display: inline-block;
-    width: 4px;
-    height: 4px;
-    margin-left: -2px;
-    border-radius: 50%;
-}
-  div {
-    display: block;
-}
-  .timeline {
-    position: relative;
-    list-style: none;
-    padding-left: 0;
-    z-index: 1;
-}
-  ol {
-    display: block;
-    list-style-type: decimal;
-    margin-block-start: 1em;
-    margin-block-end: 1em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    padding-inline-start: 40px;
-}
-  .theme--light .v-card, .application .theme--light.v-card {
-    background-color: #fff;
-    color: rgba(0,0,0,0.87);
-}
-  .layout.row {
-    -webkit-box-orient: horizontal;
-    -webkit-box-direction: normal;
-    -ms-flex-direction: row;
-    flex-direction: row;
-}
-  .application--wrap {
-    -webkit-box-flex: 1;
-    -ms-flex: 1 1 auto;
-    flex: 1 1 auto;
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    min-height: 100vh;
-    max-width: 100%;
-    position: relative;
-}
-::selection {
-    background-color: #b3d4fc;
-    color: #000;
-    text-shadow: none;
-}
-  .timeline-block {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-}
-  li {
-    display: list-item;
-    text-align: -webkit-match-parent;
-}
-  .timeline {
-    position: relative;
-    list-style: none;
-    padding-left: 0;
-    z-index: 1;
-}
-  .application.theme--light {
-    background: #fafafa;
-    color: rgba(0,0,0,0.87);
-}
-  .application {
-    font-family: 'Roboto', sans-serif;
-    line-height: 1.5;
-}
-  .timeline-point-sm .timeline-point {
-    width: 64px;
-}
-  .v-card > *:last-child:not(.v-btn):not(.v-chip) {
-    border-bottom-left-radius: inherit;
-    border-bottom-right-radius: inherit;
-}
-  .v-card__text {
-    padding: 16px;
-    width: 100%;
-}
-  .white {
-    background-color: #fff !important;
-    border-color: #fff !important;
-}
-  * {
-    background-repeat: no-repeat;
-    padding: 0;
-    margin: 0;
-}
-  .timeline.timeline-content-right .timeline-block .timeline-content {
-    -webkit-box-ordinal-group: 4;
-    -ms-flex-order: 3;
-    order: 3;
-}
-  .timeline.timeline-activity .timeline-content {
-    margin-bottom: 7px;
-}
-  .timeline-content {
-    -webkit-box-flex: 1;
-    -ms-flex: 1 1 0%;
-    flex: 1 1 0%;
-    margin-bottom: 60px;
-}
+<style lang="css">
+  @import '../assets/css/profile.css';
 </style>
