f4a8e2846b2dee04042b47be8dce4e4422d97193
Smaker <jiheon.unidev@gmail.com>
2018-12-12 23:11:51 +0900

    Update WriteView

--- frontend/src/components/Post/WriteView.vue
+++ frontend/src/components/Post/WriteView.vue
@@ -1,20 +1,23 @@
 <template>
     <v-container>
-        <v-layout>
-            <h1>Write</h1>
-            <v-spacer></v-spacer>
-            <v-btn class="success">Submit</v-btn>
-        </v-layout>
-        <v-text-field v-model="title" label="title" required></v-text-field>
-        <v-text-field v-model="sha256" label="sha256" required></v-text-field>
-        <markdown-editor preview-class="markdown-body" v-model="content" ref="markdownEditor"></markdown-editor>
-
-        <br>
+        <v-card>
+            <v-container>
+                <v-layout>
+                    <h1>Write</h1>
+                    <v-spacer></v-spacer>
+                    <v-btn class="success">Submit</v-btn>
+                </v-layout>
+                <v-text-field v-model="title" label="title" required></v-text-field>
+                <v-text-field v-model="sha256" label="sha256" required></v-text-field>
+                <markdown-editor preview-class="markdown-body" v-model="content" ref="markdownEditor"></markdown-editor>
+            </v-container>
+            <br>
+        </v-card>
     </v-container>
 </template>
 
 <script>
-    import hljs from 'highlight.js';
+    import hljs from 'highlight.js'
 
     window.hljs = hljs;
     import markdownEditor from 'vue-simplemde/src/markdown-editor'
