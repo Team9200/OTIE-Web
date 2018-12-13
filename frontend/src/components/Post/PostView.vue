f80c2b9255c1df35ed340d55487c954c4b7ffdf7
Smaker <jiheon.unidev@gmail.com>
2018-12-13 23:14:38 +0900

    Update Markdown Editor

--- frontend/src/components/Post/PostView.vue
+++ frontend/src/components/Post/PostView.vue
@@ -5,16 +5,16 @@
                 <h2>{{ post.title }}</h2>
             </v-card-title>
             <v-card-text v-if="post.body !== undefined">
-                    <!-- <li>md5: {{ post.body.md5 }}</li>
+                <!-- <li>md5: {{ post.body.md5 }}</li>
                     <li>sha1: {{ post.body.sha1 }}</li>
                     <li>sha256: {{ post.body.sha256 }}</li> -->
-                    <!-- <li>collector: {{ post.body.collector }}</li> -->
-                    <div class="post-text">분석가: {{ post.body.analyzer }}</div>
-                    <v-divider></v-divider>
-                    <div class="post-text">날짜: {{ getDate(post.timestamp) }}</div>
-                    <v-divider></v-divider>
-                    <v-btn @click="go(post)" style="color: white;" color="green">Malware Info ({{ post.body.sha256 }})</v-btn>
-                    <!-- <li>first_seen: {{ post.body.first_seen }}</li> -->
+                <!-- <li>collector: {{ post.body.collector }}</li> -->
+                <div class="post-text">분석가: {{ post.body.analyzer }}</div>
+                <v-divider></v-divider>
+                <div class="post-text">날짜: {{ getDate(post.timestamp) }}</div>
+                <v-divider></v-divider>
+                <v-btn @click="go(post)" style="color: white;" color="green">Malware Info ({{ post.body.sha256 }})</v-btn>
+                <!-- <li>first_seen: {{ post.body.first_seen }}</li> -->
             </v-card-text>
         </v-card>
         <br>
@@ -24,7 +24,7 @@
             </v-card-title> -->
             <v-card-text v-if="post.body !== undefined">
                 <!-- <v-container v-if="post.body !== undefined"> -->
-                    <vue-markdown>{{ post.body.description }}</vue-markdown>
+                <vue-markdown>{{ post.body.description }}</vue-markdown>
                 <!-- </v-container> -->
             </v-card-text>
         </v-card>
@@ -62,7 +62,7 @@
 </template>
 
 <script>
-import hljs from 'highlight.js'
+    import hljs from 'highlight.js'
 
     window.hljs = hljs;
     import markdownEditor from 'vue-simplemde/src/markdown-editor'
@@ -111,13 +111,23 @@ import hljs from 'highlight.js'
     }
 </script>
 
-<style> 
+<style>
     @import '~simplemde/dist/simplemde.min.css';
     @import '~github-markdown-css';
     @import '~highlight.js/styles/atom-one-dark.css';
+
     .post-text {
         font-size: 15px;
         padding-top: 5px;
         padding-bottom: 5px;
+    }
+
+    .markdown-editor .CodeMirror,
+    .markdown-editor .CodeMirror-scroll {
+        min-height: 100px;
+    }
+
+    .markdown-editor .CodeMirror {
+        height: 100px;
     }
 </style>
